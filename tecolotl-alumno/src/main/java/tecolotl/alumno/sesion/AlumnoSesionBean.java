922f36e5c1e6445576f6f9149242fbef1948f564
taller <taller@taller-Lenovo-G470>
2018-11-16 13:53:29 -0600

    insertar Alumno

--- tecolotl-alumno/src/main/java/tecolotl/alumno/sesion/AlumnoSesionBean.java
+++ tecolotl-alumno/src/main/java/tecolotl/alumno/sesion/AlumnoSesionBean.java
@@ -1,5 +1,6 @@
 package tecolotl.alumno.sesion;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.LocalBean;
@@ -9,6 +10,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import tecolotl.alumno.persistencia.entidad.AlumnoEntidad;
+import tecolotl.alumno.persistencia.entidad.GradoEscolarEntidad;
 
 /**
  * Session Bean implementation class AlumnoSesionBean
@@ -24,5 +26,31 @@ public class AlumnoSesionBean {
 	public List<AlumnoEntidad> bucar() {
 		Query query = entityManager.createQuery("SELECT a FROM AlumnoEntidad a");
 		return (List<AlumnoEntidad>)query.getResultList();
+	}
+	
+	
+	public void insertar (int gradoEscolar, String nombre, String apellidopaterno, String apellidomaterno, String apodo, String correoelectronico, String contrasenia,String nivelingles, Date nacimiento,String correo_padre_familia,String contraseniapadrefamilia) {
+		AlumnoEntidad alumnoEntidad = new AlumnoEntidad();
+		GradoEscolarEntidad gradoEscolarEntidad = new GradoEscolarEntidad();
+		gradoEscolarEntidad.setId(gradoEscolar);
+		alumnoEntidad.setGradoEscolarBean(gradoEscolarEntidad);
+		 alumnoEntidad.setApellidoPaterno(apellidopaterno);
+		 alumnoEntidad.setApellidoMaterno(apellidomaterno);
+		 alumnoEntidad.setNombre(nombre);
+		 alumnoEntidad.setApodo(apodo);
+		 alumnoEntidad.setCorreoElectronico(correoelectronico);
+		 alumnoEntidad.setContrasenia(contrasenia);
+		 alumnoEntidad.setNivelIngles(nivelingles);
+		 alumnoEntidad.setNacimiento(nacimiento);
+		 alumnoEntidad.setCorreoPadreFamilia(correo_padre_familia);
+		 alumnoEntidad.setContraseniaPadreFamilia(contraseniapadrefamilia);
+		 
+		 
+		 entityManager.persist(alumnoEntidad);
+		
+		
+		
+		
+		
 	}
 }
--- tecolotl-alumno/src/test/java/tecolotl/alumno/sesion/AlumnoSesionBeanTest.java
+++ tecolotl-alumno/src/test/java/tecolotl/alumno/sesion/AlumnoSesionBeanTest.java
@@ -1,12 +1,47 @@
 package tecolotl.alumno.sesion;
 
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+
+import tecolotl.alumno.persistencia.entidad.AlumnoEntidad;
+import tecolotl.alumno.persistencia.entidad.AlumnoEntidadTest;
+import tecolotl.alumno.persistencia.entidad.GradoEscolarEntidad;
+
 public class AlumnoSesionBeanTest {
-	/*
-    @Deployment
-    public static Archive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-    }
-    */
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	@Deployment
+	public static Archive<?> createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "test.war")
+                .addPackage(AlumnoSesionBean.class.getPackage())
+                .addPackage(AlumnoEntidad.class.getPackage())
+                .addAsResource("META-INF/persistence.xml")
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}   
+	@Inject
+		private AlumnoSesionBean alumnoSesionBean;
+	    @Test
+	    
+	    public void insertar() {
+	    	Date date = new Date();
+	    	alumnoSesionBean.insertar(5 ,"Guillermo", "Cuahuey","Estrada", "Memo", "cu.es.guillermo@nahtechnology.com", "pass", "a1", date , "guillermo@gamil.com", "pass");
+	    	  }
+	    public void crear() {
+
+	    }
 
 }
