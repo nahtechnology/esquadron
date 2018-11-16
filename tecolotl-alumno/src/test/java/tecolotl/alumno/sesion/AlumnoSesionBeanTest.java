c46024298cd939758b88d6100360c379c62aa57b
taller <taller@taller-Lenovo-G470>
2018-11-16 16:58:27 -0600

    Error insertar

--- tecolotl-alumno/src/test/java/tecolotl/alumno/sesion/AlumnoSesionBeanTest.java
+++ tecolotl-alumno/src/test/java/tecolotl/alumno/sesion/AlumnoSesionBeanTest.java
@@ -10,17 +10,20 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import tecolotl.alumno.persistencia.entidad.AlumnoEntidad;
 import tecolotl.alumno.persistencia.entidad.AlumnoEntidadTest;
 import tecolotl.alumno.persistencia.entidad.GradoEscolarEntidad;
 
+@RunWith(Arquillian.class)
 public class AlumnoSesionBeanTest {
 	private Logger logger = Logger.getLogger(getClass().getName());
 	
@@ -40,8 +43,10 @@ public class AlumnoSesionBeanTest {
 	    	Date date = new Date();
 	    	alumnoSesionBean.insertar(5 ,"Guillermo", "Cuahuey","Estrada", "Memo", "cu.es.guillermo@nahtechnology.com", "pass", "a1", date , "guillermo@gamil.com", "pass");
 	    	  }
+	   
 	    public void crear() {
 
 	    }
+	   
 
 }
