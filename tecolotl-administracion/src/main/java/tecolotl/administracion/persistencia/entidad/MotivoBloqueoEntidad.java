0c48e0cdac520178f5c0a862305b15d7d80d3410
antonio <antonio@debianita>
2018-11-26 17:33:44 -0600

    Motivo de Bloqueo con pruebas unitarias

---
+++ tecolotl-administracion/src/main/java/tecolotl/administracion/persistencia/entidad/MotivoBloqueoEntidad.java
@@ -0,0 +1,41 @@
+package tecolotl.administracion.persistencia.entidad;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "motivo_bloqueo", schema ="administracion")
+@NamedQuery(name = "MotivoBloqueoEntidad.busca", query = "SELECT m FROM MotivoBloqueoEntidad m")
+public class MotivoBloqueoEntidad {
+
+	private short id;
+	private String descripcion;
+	
+	@Id
+	public short getId() {
+		return id;
+	}
+	
+	public void setId(short id) {
+		this.id = id;
+	}
+	
+	@Basic
+	@Column(name = "descripcion")
+	@NotNull
+	@Size(min = 3, max = 20)
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
