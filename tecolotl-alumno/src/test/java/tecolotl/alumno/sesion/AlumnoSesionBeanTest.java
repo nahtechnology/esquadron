a4f88bba72e9055ac395dfba011a1c39ad7c1b1f
guillermo <guillermo@debian>
2018-11-26 13:01:45 -0600

    1. probar query @NamedQuery(name = "AlumnoEntidad.buscaContrasenia", query = "SELECT a FROM AlumnoEntidad a WHERE a.apodo=:apodo AND a.contrasenia=:contrasenia") (tecolotl.alumno.persistencia.entidad).
    2. probar el metodo "buscar"(tecolotl.alumno.sesion).
    
    3. Documentar metodo 2 buscar" @NamedQuery(name =
    "AlumnoEntidad.buscaContrasenia", query = "SELECT a FROM AlumnoEntidad a
    WHERE a.apodo=:apodo AND a.contrasenia=:contrasenia").
    
    4. Eliminar query @NamedQuery(name = "AlumnoEntidad.buscarId", query=
    "SELECT t FROM AlumnoEntidad t WHERE t.nombre=:nombre AND
    t.contrasenia=:password") (tecolotl.alumno.persistencia.entidad).

--- tecolotl-alumno/src/test/java/tecolotl/alumno/sesion/AlumnoSesionBeanTest.java
+++ tecolotl-alumno/src/test/java/tecolotl/alumno/sesion/AlumnoSesionBeanTest.java
@@ -1,19 +1,28 @@
 package tecolotl.alumno.sesion;
 
 import java.util.Date;
+import java.util.List;
+import java.util.logging.Logger;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
 import org.junit.Test;
 
 import tecolotl.alumno.persistencia.entidad.AlumnoEntidad;
+import tecolotl.alumno.persistencia.entidad.AlumnoEntidadTest;
+import tecolotl.alumno.persistencia.entidad.GradoEscolarEntidad;
 
 public class AlumnoSesionBeanTest {
+	private Logger logger = Logger.getLogger(getClass().getName());
 	
 	@Deployment
 	public static Archive<?> createDeployment() {
@@ -22,14 +31,31 @@ public class AlumnoSesionBeanTest {
                 .addPackage(AlumnoEntidad.class.getPackage())
                 .addAsResource("META-INF/persistence.xml")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-	}   
+	} 
+    @PersistenceContext()
+    private EntityManager entityManager;
 	@Inject
 	private AlumnoSesionBean alumnoSesionBean;
+
 
 	@Test
 	public void insertar() {
 		Date date = new Date();
 		alumnoSesionBean.insertar(5 ,"Guillermo", "Cuahuey","Estrada", "Memo", "cu.es.guillermo@nahtechnology.com", "pass", "a1", date , "guillermo@gamil.com", "pass");
 	}
+	
+	@Test
+	public void buscar() {
+		Assert.assertTrue("Se no se ncontro el Alumno", alumnoSesionBean.busca("ESMERALDA", "ESMERALDA"));
+	}
+    @Test
+    public void buscarContrasenia() {
+    	TypedQuery<AlumnoEntidad> typedQuery = entityManager.createQuery("AlumnoEntidad.buscaContrasenia", AlumnoEntidad.class);
+    	typedQuery.setParameter("apodo", "ESMERALDA");
+    	typedQuery.setParameter("contrasenia", "contrasenia");
+    	List<AlumnoEntidad> listaalumnos = typedQuery.getResultList();
+    	Assert.assertNotNull("No encontro Alumno", listaalumnos );
+    	
+    }
 
 }
