48ba37e51beb5cfb3368565490104bf1ba669f69
nahtechnology <antonio.alonso@nahtechnology.com>
2019-06-21 08:15:38 -0500

    Se agregan archivos de avatar, icono y convertidor para coordinador

---
+++ tecolotl-web/src/main/java/tecolotl/web/herramienta/ContraseniaConvertidor.java
@@ -0,0 +1,24 @@
+package tecolotl.web.herramienta;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+@FacesConverter(value = "tecolotl.web.ContraseniaConvertidor")
+public class ContraseniaConvertidor implements Converter {
+
+    Charset charset = StandardCharsets.UTF_8;
+
+    @Override
+    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        return value == null ? null : value.getBytes(charset);
+    }
+
+    @Override
+    public String getAsString(FacesContext context, UIComponent component, Object value) {
+        return value == null ? "" : new String((byte[])value, charset);
+    }
+}
