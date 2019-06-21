a2debeea03b276ff69f90fb834efdb6ddde1d562
nahtechnology <antonio.alonso@nahtechnology.com>
2019-06-21 06:41:11 -0500

    Coordinador motivo de bloqueo con error

--- tecolotl-web/src/main/java/tecolotl/web/administracion/controlador/CoordinadorControlador.java
+++ tecolotl-web/src/main/java/tecolotl/web/administracion/controlador/CoordinadorControlador.java
@@ -1,6 +1,8 @@
 package tecolotl.web.administracion.controlador;
 
 import tecolotl.administracion.modelo.coordinador.CoordinadorModelo;
+import tecolotl.administracion.modelo.coordinador.CoordinadorMotivoBloqueoModelo;
+import tecolotl.administracion.sesion.CoordinadorMotivoBloqueoSesionBean;
 import tecolotl.administracion.sesion.CoordinadorSesionBean;
 import tecolotl.web.controlador.TablaControlador;
 
@@ -11,6 +13,7 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 @ViewScoped
 @Named
@@ -19,12 +22,54 @@ public class CoordinadorControlador extends TablaControlador<CoordinadorModelo> 
     @Inject
     private CoordinadorSesionBean coordinadorSesionBean;
 
+    @Inject
+    private CoordinadorMotivoBloqueoSesionBean coordinadorMotivoBloqueoSesionBean;
+
+    private List<CoordinadorMotivoBloqueoModelo> coordinadorMotivoBloqueoModeloLista;
     private CoordinadorModelo coordinadorModelo;
+    private CoordinadorMotivoBloqueoModelo coordinadorMotivoBloqueoModelo;
     private String claveCentroTrabajo;
 
     @PostConstruct
     public void init() {
         claveCentroTrabajo = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("escuela");
+        coordinadorMotivoBloqueoModeloLista = coordinadorMotivoBloqueoSesionBean.buscaFiltro("Sin Bloqueo");
+        actualizaDataModel();
+    }
+
+    public void limpiaCoordinador() {
+        coordinadorModelo = new CoordinadorModelo();
+    }
+
+    public void agrega() {
+        coordinadorModelo.setClaveCentroTrabajo(claveCentroTrabajo);
+        coordinadorSesionBean.agreaga(coordinadorModelo);
+        limpiaCoordinador();
+        actualizaDataModel();
+    }
+
+    public void elimina() {
+        coordinadorSesionBean.elimina(coordinadorModelo);
+        limpiaCoordinador();
+        actualizaDataModel();
+    }
+
+    public void actualiza() {
+        coordinadorSesionBean.actualiza(coordinadorModelo);
+        limpiaCoordinador();
+        actualizaDataModel();
+    }
+
+    public void bloqueo() {
+        coordinadorModelo.setClaveCentroTrabajo(claveCentroTrabajo);
+        coordinadorSesionBean.estatus(coordinadorModelo, coordinadorMotivoBloqueoModelo.getClave());
+        limpiaCoordinador();
+        actualizaDataModel();
+    }
+
+    public void desbloquea() {
+        coordinadorSesionBean.estatus(coordinadorModelo, (short)1);
+        limpiaCoordinador();
         actualizaDataModel();
     }
 
@@ -39,5 +84,21 @@ public class CoordinadorControlador extends TablaControlador<CoordinadorModelo> 
 
     public void setCoordinadorModelo(CoordinadorModelo coordinadorModelo) {
         this.coordinadorModelo = coordinadorModelo;
+    }
+
+    public List<CoordinadorMotivoBloqueoModelo> getCoordinadorMotivoBloqueoModeloLista() {
+        return coordinadorMotivoBloqueoModeloLista;
+    }
+
+    public void setCoordinadorMotivoBloqueoModeloLista(List<CoordinadorMotivoBloqueoModelo> coordinadorMotivoBloqueoModeloLista) {
+        this.coordinadorMotivoBloqueoModeloLista = coordinadorMotivoBloqueoModeloLista;
+    }
+
+    public CoordinadorMotivoBloqueoModelo getCoordinadorMotivoBloqueoModelo() {
+        return coordinadorMotivoBloqueoModelo;
+    }
+
+    public void setCoordinadorMotivoBloqueoModelo(CoordinadorMotivoBloqueoModelo coordinadorMotivoBloqueoModelo) {
+        this.coordinadorMotivoBloqueoModelo = coordinadorMotivoBloqueoModelo;
     }
 }
