838334a11046d6aec6d74da33110706d9b7a225f
nahtechnology <antonio.alonso@nahtechnology.com>
2019-07-05 16:50:19 -0500

    Se arreglan los errores de las clases de pruebas de cifrado

--- tecolotl-web/src/main/java/tecolotl/web/administracion/controlador/EscuelaPoblacionControlador.java
+++ tecolotl-web/src/main/java/tecolotl/web/administracion/controlador/EscuelaPoblacionControlador.java
@@ -1,14 +1,34 @@
 package tecolotl.web.administracion.controlador;
 
+import tecolotl.administracion.modelo.escuela.EscuelaPoblacionModelo;
+import tecolotl.administracion.sesion.LicenciaSesionBean;
+import tecolotl.profesor.sesion.ProfesorSesionBean;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @RequestScoped
 @Named
 public class EscuelaPoblacionControlador {
 
+    @Inject
+    private ProfesorSesionBean profesorSesionBean;
+
+    @Inject
+    private LicenciaSesionBean licenciaSesionBean;
+
+    private String claveCentroTrabajo;
     private int totalAlumnos;
     private int totalProfesores;
+    private int totalLicencias;
+
+    public void actualiza() {
+        EscuelaPoblacionModelo escuelaPoblacionModelo = profesorSesionBean.total(claveCentroTrabajo);
+        totalAlumnos = escuelaPoblacionModelo.getTotalAlumnos();
+        totalProfesores = escuelaPoblacionModelo.getTotalProfesores();
+        totalLicencias = licenciaSesionBean.cuenta(claveCentroTrabajo);
+    }
 
     public int getTotalAlumnos() {
         return totalAlumnos;
@@ -24,5 +44,21 @@ public class EscuelaPoblacionControlador {
 
     public void setTotalProfesores(int totalProfesores) {
         this.totalProfesores = totalProfesores;
+    }
+
+    public int getTotalLicencias() {
+        return totalLicencias;
+    }
+
+    public void setTotalLicencias(int totalLicencias) {
+        this.totalLicencias = totalLicencias;
+    }
+
+    public String getClaveCentroTrabajo() {
+        return claveCentroTrabajo;
+    }
+
+    public void setClaveCentroTrabajo(String claveCentroTrabajo) {
+        this.claveCentroTrabajo = claveCentroTrabajo;
     }
 }
