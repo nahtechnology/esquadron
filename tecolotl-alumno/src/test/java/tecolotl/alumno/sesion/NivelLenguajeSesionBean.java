c20e7559ea191acde7dbf3646f860e7d5e5b5d74
nahtechnology <antonio.alonso@nahtechnology.com>
2019-08-26 17:50:47 -0500

    Se arrega Nivel leanguaje sesion bean

---
+++ tecolotl-alumno/src/test/java/tecolotl/alumno/sesion/NivelLenguajeSesionBean.java
@@ -0,0 +1,11 @@
+package tecolotl.alumno.sesion;
+
+import tecolotl.nucleo.modelo.CatalogoModelo;
+import tecolotl.nucleo.persistencia.entidad.CatalagoEntidad;
+import tecolotl.nucleo.sesion.CatalogoSesionBean;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class NivelLenguajeSesionBean extends CatalogoSesionBean<CatalogoModelo, CatalagoEntidad> {
+}
