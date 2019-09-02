4001db197644e003069e4fd12fef29dc65056ed6
Mauricio <maugg76@gmail.com>
2019-09-02 17:50:55 -0500

    transcript

---
+++ tecolotl-web/src/main/java/tecolotl/web/alumno/GlosarioControlador.java
@@ -0,0 +1,43 @@
+package tecolotl.web.alumno;
+
+import tecolotl.alumno.modelo.glosario.GlosarioModelo;
+import tecolotl.alumno.sesion.GlosarioSesionBean;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@RequestScoped
+@Named
+public class GlosarioControlador {
+    private List<GlosarioModelo> glosarioModeloLista;
+    private GlosarioModelo glosarioModelo;
+
+    @Inject
+    private GlosarioSesionBean glosarioSesionBean;
+
+    @PostConstruct
+    public void init(){
+        String idAvtividad = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("idActividad");
+        glosarioModeloLista = glosarioSesionBean.busca(idAvtividad);
+    }
+
+    public List<GlosarioModelo> getGlosarioModeloLista() {
+        return glosarioModeloLista;
+    }
+
+    public void setGlosarioModeloLista(List<GlosarioModelo> glosarioModeloLista) {
+        this.glosarioModeloLista = glosarioModeloLista;
+    }
+
+    public GlosarioModelo getGlosarioModelo() {
+        return glosarioModelo;
+    }
+
+    public void setGlosarioModelo(GlosarioModelo glosarioModelo) {
+        this.glosarioModelo = glosarioModelo;
+    }
+}
