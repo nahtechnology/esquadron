7a165538d69c8836313896ba3f8cb46d456159f8
Mauricio <maugg76@gmail.com>
2019-09-25 14:35:44 -0500

    mas actividad

---
+++ tecolotl-web/src/main/java/tecolotl/web/alumno/CompletarOracionesControlador.java
@@ -0,0 +1,42 @@
+package tecolotl.web.alumno;
+
+import tecolotl.alumno.modelo.completar.CompletarModelo;
+import tecolotl.alumno.modelo.completar.TareaCompletarModelo;
+import tecolotl.alumno.sesion.CompletarSesionBean;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RequestScoped
+@Named
+public class CompletarOracionesControlador {
+
+    private Logger logger;
+
+    @Inject
+    private CompletarSesionBean completarSesionBean;
+
+    @Inject
+    private AlumnoControlador alumnoControlador;
+
+    private List<TareaCompletarModelo> tareaCompletarModeloLista;
+
+    @PostConstruct
+    public void init(){
+        tareaCompletarModeloLista = completarSesionBean.busca(alumnoControlador.getTareaActividadModelo().getId());
+        logger.info(tareaCompletarModeloLista.toString());
+    }
+
+
+    public List<TareaCompletarModelo> getTareaCompletarModeloLista() {
+        return tareaCompletarModeloLista;
+    }
+
+    public void setTareaCompletarModeloLista(List<TareaCompletarModelo> tareaCompletarModeloLista) {
+        this.tareaCompletarModeloLista = tareaCompletarModeloLista;
+    }
+}
