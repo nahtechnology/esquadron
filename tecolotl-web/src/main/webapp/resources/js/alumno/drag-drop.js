5174e51b807c096dfa8e6b1fde8ff25dfb9846ef
Mauricio <maugg76@gmail.com>
2019-10-03 17:59:53 -0500

    errores de transcrip ordenar

---
+++ tecolotl-web/src/main/webapp/resources/js/alumno/drag-drop.js
@@ -0,0 +1,16 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev, el) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    el.appendChild(document.getElementById(data));
+}
+
+
+
