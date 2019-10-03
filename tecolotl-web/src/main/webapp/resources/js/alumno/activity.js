8b0d5daeeb529afb172889ac82fb4b088712d99d
nahtechnology <antonio.alonso@nahtechnology.com>
2019-10-03 15:24:55 -0500

    Ahora muestra una mensaje de vacio cuando no hay mapa mental

--- tecolotl-web/src/main/webapp/resources/js/alumno/activity.js
+++ tecolotl-web/src/main/webapp/resources/js/alumno/activity.js
@@ -1,5 +1,6 @@
 var menu = document.querySelector('.uk-container .uk-subnav');
 var video = document.querySelector('.uk-container .js-filter');
+var relacionar = document.querySelector('.uk-container .match');
 
 document.addEventListener('DOMContentLoaded', function (evt) {
     video.querySelectorAll('#scrollTranscript p').forEach(function (parrafo) {
