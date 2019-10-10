cf7b0de9695acfeb599ad02d5c46887ef997a07f
juan <juancarlos.flores@nahtechnology.com>
2019-10-09 23:47:02 -0500

    media queries en mapa 3

--- tecolotl-web/src/main/java/tecolotl/web/alumno/AlumnoControlador.java
+++ tecolotl-web/src/main/java/tecolotl/web/alumno/AlumnoControlador.java
@@ -32,8 +32,8 @@ public class AlumnoControlador implements Serializable {
 
     @PostConstruct
     public void init() {
-        alumnoModelo = alumnoSesionBean.busca(1);
-        tareaActvidadModeloLista = tareaSesionBean.buscaActividad(1);
+        alumnoModelo = alumnoSesionBean.busca(2);
+        tareaActvidadModeloLista = tareaSesionBean.buscaActividad(2);
     }
 
     public String seleccion(TareaActividadModelo tareaActividadModelo){
