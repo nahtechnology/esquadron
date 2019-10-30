2ad6f632ad7fad4f73752a283f14db793d9eb715
nahtechnology <antonio.alonso@nahtechnology.com>
2019-10-29 19:00:18 -0600

    se agrega el controlador

---
+++ tecolotl-web/src/main/java/tecolotl/web/profesor/ProfesorGrupoControlador.java
@@ -0,0 +1,50 @@
+package tecolotl.web.profesor;
+
+import tecolotl.alumno.modelo.AlumnoModelo;
+import tecolotl.alumno.sesion.AlumnoSesionBean;
+import tecolotl.profesor.modelo.GrupoModelo;
+import tecolotl.profesor.sesion.GrupoSesionBean;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@SessionScoped
+@Named
+public class ProfesorGrupoControlador implements Serializable {
+
+    @Inject
+    private GrupoSesionBean grupoSesionBean;
+
+    @Inject
+    private AlumnoSesionBean alumnoSesionBean;
+
+    private GrupoModelo grupoModelo;
+    private AlumnoModelo alumnoModelo;
+
+    public void detalleGrupo(@NotNull Integer idGrupo) {
+        grupoModelo = grupoSesionBean.buscaId(idGrupo);
+    }
+
+    public void detalleAlumno(@NotNull Integer idAlumno) {
+        alumnoModelo = alumnoSesionBean.busca(idAlumno);
+    }
+
+    public GrupoModelo getGrupoModelo() {
+        return grupoModelo;
+    }
+
+    public void setGrupoModelo(GrupoModelo grupoModelo) {
+        this.grupoModelo = grupoModelo;
+    }
+
+    public AlumnoModelo getAlumnoModelo() {
+        return alumnoModelo;
+    }
+
+    public void setAlumnoModelo(AlumnoModelo alumnoModelo) {
+        this.alumnoModelo = alumnoModelo;
+    }
+}
