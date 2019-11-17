31f28ac35f7963d9c899c3041e64f5f6a737e89c
nahtechnology <antonio.alonso@nahtechnology.com>
2019-11-16 22:24:25 -0600

    Se cambio las actividades de check a radio button

--- tecolotl-web/src/main/java/tecolotl/web/profesor/AsignarActividadControlador.java
+++ tecolotl-web/src/main/java/tecolotl/web/profesor/AsignarActividadControlador.java
@@ -1,12 +1,14 @@
 package tecolotl.web.profesor;
 
 import tecolotl.profesor.modelo.ActividadModelo;
+import tecolotl.profesor.sesion.GrupoAlumnoSesionBean;
 import tecolotl.profesor.sesion.TareasAlumnoSesionBean;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -22,11 +24,23 @@ public class AsignarActividadControlador {
     @Inject
     private TareasAlumnoSesionBean tareasAlumnoSesionBean;
 
+    @Inject
+    private GrupoAlumnoSesionBean grupoAlumnoSesionBean;
+
     private List<ActividadModelo> actividadModeloLista;
+
+    @NotNull
     private Integer grupo;
+
+    @NotNull
+    private String actividad;
 
     public void inicio() {
         actividadModeloLista = tareasAlumnoSesionBean.busca(grupo);
+    }
+
+    public void asiganarTarea() {
+        grupoAlumnoSesionBean.tarea(grupo, actividad);
     }
 
     public List<String> buscaNiveles() {
@@ -47,5 +61,13 @@ public class AsignarActividadControlador {
 
     public void setGrupo(Integer grupo) {
         this.grupo = grupo;
+    }
+
+    public String getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
     }
 }
