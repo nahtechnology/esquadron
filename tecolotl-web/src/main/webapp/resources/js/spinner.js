d0d6263e2e4f042e375e3a7a9114f082e84d83b8
Juan <juancarlos04>
2019-11-16 18:59:28 -0600

    estilos al spinner s-quadron

--- tecolotl-web/src/main/webapp/resources/js/spinner.js
+++ tecolotl-web/src/main/webapp/resources/js/spinner.js
@@ -1,5 +1,6 @@
 window.onload = function() {
    var listo = document.getElementById('cargado');
    listo.classList.remove('spinner-open');
+   listo.style.display="none";
    console.log("pagina cargada");
  };
