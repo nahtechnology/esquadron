12cf21ef3be07d8040cffaddcac25b19ccaf5eb4
nahtechnology <antonio.alonso@nahtechnology.com>
2020-01-02 16:13:04 -0600

    Ahora trae las contrasenias de la base de datos (objetos faltantes)

---
+++ tecolotl-web/src/main/java/tecolotl/web/profesor/AlumnoGrupoControlador.java
@@ -0,0 +1,41 @@
+package tecolotl.web.profesor;
+
+import tecolotl.alumno.modelo.AlumnoModelo;
+import tecolotl.profesor.sesion.GrupoAlumnoSesionBean;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RequestScoped
+@Named
+public class AlumnoGrupoControlador {
+
+    @Inject
+    private GrupoAlumnoSesionBean grupoAlumnoSesionBean;
+
+    @Inject
+    private ProfesorGrupoControlador profesorGrupoControlador;
+
+    @Inject
+    private Logger logger;
+
+    private List<AlumnoModelo> alumnoModeloLista;
+
+    @PostConstruct
+    public void inicio() {
+        alumnoModeloLista = grupoAlumnoSesionBean.detalleAlumnos(profesorGrupoControlador.getGrupoModelo().getId());
+        logger.info(alumnoModeloLista.toString());
+    }
+
+    public List<AlumnoModelo> getAlumnoModeloLista() {
+        return alumnoModeloLista;
+    }
+
+    public void setAlumnoModeloLista(List<AlumnoModelo> alumnoModeloLista) {
+        this.alumnoModeloLista = alumnoModeloLista;
+    }
+}
