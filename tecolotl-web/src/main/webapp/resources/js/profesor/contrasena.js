c1152091d255f51f2578f3f60ddc71ce984ef2df
mauricio <maugg76@gmail.com>
2020-01-05 13:46:50 -0600

    contrasena coordenadas correctas

--- tecolotl-web/src/main/webapp/resources/js/profesor/contrasena.js
+++ tecolotl-web/src/main/webapp/resources/js/profesor/contrasena.js
@@ -5,33 +5,34 @@ var contenedorPassword = document.querySelector('#modal-password .uk-modal-body'
 var passwords = contenedorPassword.querySelectorAll('p > span');
 
 passwords.forEach(function (contra,indice) {
-    var img = 0;
 
     var cadena = contra.innerHTML.split(',');
     contra.innerHTML = "";
     cadena.forEach(function (cuenta) {
+        let img = 0;
         let cadenita;
         cadenita = cuenta.split(':');
         console.log(cadenita);
         cadenita.forEach(function (corde,index) {
             var imagen = document.createElement('img');
             if(index === 0){
+                img+=parseInt(corde);
+                console.log(img);
+            }else {
                 switch (parseInt(corde)) {
                     case 0:
-                        img = 1;
+                        img += 1;
                         break;
                     case 1:
-                        img = 8;
+                        img += 8;
                         break;
                     case 2:
-                        img = 15;
+                        img += 15;
                         break;
                     case 3:
-                        img = 22;
+                        img += 22;
                         break;
                 }
-            }else {
-                img+=parseInt(corde);
                 console.log(img);
                 imagen.src = "../resources/img/alumno/iconos-login/" + img + ".svg";
                 imagen.style.marginLeft = "5px";
