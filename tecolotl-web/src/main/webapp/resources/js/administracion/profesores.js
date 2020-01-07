f8e502b5b7ef1b0eaff03c44f783db9393a8ce98
Juan <juancarlos04>
2020-01-07 11:14:46 -0600

    se implenta js en profesores para ocultar modal

--- tecolotl-web/src/main/webapp/resources/js/administracion/profesores.js
+++ tecolotl-web/src/main/webapp/resources/js/administracion/profesores.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', ejecucionInicio);
 
 function ejecucionInicio(evento) {
     modalProfresor = document.getElementById('modal-profesor');
+    modalProfresor.querySelector('.uk-modal-footer button').addEventListener("click",cerrarModal);
+}
+
+function cerrarModal() {
+    UIkit.modal(modalProfresor).hide();
 }
 
 
