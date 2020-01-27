6fded899e15326c022d61576193c06bc934de634
Jesus <sauronavp92@hotmail.com>
2020-01-27 17:40:35 -0600

    Vista Ciclos Escolares listo para pruebas.

--- tecolotl-web/src/main/java/tecolotl/web/administracion/controlador/CiclosEscolaresControlador.java
+++ tecolotl-web/src/main/java/tecolotl/web/administracion/controlador/CiclosEscolaresControlador.java
@@ -1,24 +1,21 @@
 package tecolotl.web.administracion.controlador;
 
-import tecolotl.administracion.sesion.EscuelaSesionBean;
-import tecolotl.administracion.sesion.LicenciaSesionBean;
 import tecolotl.profesor.modelo.CicloEscolarModelo;
 import tecolotl.profesor.sesion.CicloEscolarSessionBean;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.context.RequestScoped;
 import javax.faces.component.html.HtmlDataTable;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
-@RequestScoped
+@ViewScoped
 @Named
-public class CiclosEscolaresControlador {
+public class CiclosEscolaresControlador implements Serializable {
 
     @Inject
     private CicloEscolarSessionBean cicloEscolarSessionBean;
@@ -29,35 +26,34 @@ public class CiclosEscolaresControlador {
     private List<CicloEscolarModelo> cicloEscolarModeloLista;
     private CicloEscolarModelo cicloEscolarModelo;
     private HtmlDataTable htmlDataTable;
+    private String claveCentroTrabajo;
 
     @PostConstruct
     public void buscaCiclosEscolares() {
         cicloEscolarModelo = new CicloEscolarModelo();
-        cicloEscolarModeloLista = cicloEscolarSessionBean.busca("21DBA0014J");
+        claveCentroTrabajo = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("escuela");
+        cicloEscolarModelo.setIdEscuela(claveCentroTrabajo);
+        cicloEscolarModeloLista = cicloEscolarSessionBean.busca(claveCentroTrabajo);
+
     }
 
     public void insertaCicloEscolar() {
-        logger.info("Datos del ciclo escolar: ".concat(cicloEscolarModelo.toString()));
         cicloEscolarSessionBean.inserta(cicloEscolarModelo);
-        cicloEscolarModeloLista = cicloEscolarSessionBean.busca("21DBA0014J");
+        cicloEscolarModeloLista = cicloEscolarSessionBean.busca(claveCentroTrabajo);
     }
 
     public void seleccionaCicloEscolar() {
-        logger.info("Esto tiene el HTML DATA TABLE: ".concat(htmlDataTable.getRowData().toString()));
         cicloEscolarModelo = cicloEscolarModeloLista.get(htmlDataTable.getRowIndex());
-        logger.info("Estos son los datos actualizados: ".concat(cicloEscolarModelo.toString()));
     }
 
     public void actualizaCicloEscolar() {
-        logger.info("Datos a actualizar del Ciclo Escolar: ".concat(cicloEscolarModelo.toString()));
         cicloEscolarSessionBean.actualiza(cicloEscolarModelo);
-         cicloEscolarModeloLista = cicloEscolarSessionBean.busca("21DBA0014J");
+         cicloEscolarModeloLista = cicloEscolarSessionBean.busca(claveCentroTrabajo);
     }
 
     public void eliminaCicloEscolar() {
-        logger.info(cicloEscolarModelo.toString());
         cicloEscolarSessionBean.elimina(cicloEscolarModelo);
-        cicloEscolarModeloLista = cicloEscolarSessionBean.busca("21DBA0014J");
+        cicloEscolarModeloLista = cicloEscolarSessionBean.busca(claveCentroTrabajo);
     }
 
     public List<CicloEscolarModelo> getCicloEscolarModeloLista() {
@@ -83,5 +79,4 @@ public class CiclosEscolaresControlador {
     public void setHtmlDataTable(HtmlDataTable htmlDataTable) {
         this.htmlDataTable = htmlDataTable;
     }
-
 }
