5fa8eaea270d7d38d38f63dcbe982d49338631b4
wtfchamp <sauronavp92@hotmail.com>
2020-01-27 03:10:05 -0600

    CRUD ciclos escolares, listo para pruebas.

--- tecolotl-web/src/main/java/tecolotl/web/administracion/controlador/CiclosEscolaresControlador.java
+++ tecolotl-web/src/main/java/tecolotl/web/administracion/controlador/CiclosEscolaresControlador.java
@@ -11,7 +11,10 @@ import javax.faces.component.html.HtmlDataTable;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 @RequestScoped
 @Named
@@ -28,26 +31,33 @@ public class CiclosEscolaresControlador {
     private HtmlDataTable htmlDataTable;
 
     @PostConstruct
-    public void buscaCiclosEscolares(){
+    public void buscaCiclosEscolares() {
         cicloEscolarModelo = new CicloEscolarModelo();
         cicloEscolarModeloLista = cicloEscolarSessionBean.busca("21DBA0014J");
-        logger.info("Ciclos Escolares\n".concat(cicloEscolarModeloLista.toString()));
     }
-    public void insertaCicloEscolar(){
+
+    public void insertaCicloEscolar() {
         logger.info("Datos del ciclo escolar: ".concat(cicloEscolarModelo.toString()));
         cicloEscolarSessionBean.inserta(cicloEscolarModelo);
         cicloEscolarModeloLista = cicloEscolarSessionBean.busca("21DBA0014J");
     }
 
-    public void seleccionaCicloEscolar(){
-        this.cicloEscolarModelo = (CicloEscolarModelo) htmlDataTable.getRowData();
+    public void seleccionaCicloEscolar() {
+        logger.info("Esto tiene el HTML DATA TABLE: ".concat(htmlDataTable.getRowData().toString()));
+        cicloEscolarModelo = cicloEscolarModeloLista.get(htmlDataTable.getRowIndex());
         logger.info("Estos son los datos actualizados: ".concat(cicloEscolarModelo.toString()));
     }
-    public void actualizaCicloEscolar(){
 
+    public void actualizaCicloEscolar() {
+        logger.info("Datos a actualizar del Ciclo Escolar: ".concat(cicloEscolarModelo.toString()));
+        cicloEscolarSessionBean.actualiza(cicloEscolarModelo);
+         cicloEscolarModeloLista = cicloEscolarSessionBean.busca("21DBA0014J");
     }
-    public void eliminaCicloEscolar(){
 
+    public void eliminaCicloEscolar() {
+        logger.info(cicloEscolarModelo.toString());
+        cicloEscolarSessionBean.elimina(cicloEscolarModelo);
+        cicloEscolarModeloLista = cicloEscolarSessionBean.busca("21DBA0014J");
     }
 
     public List<CicloEscolarModelo> getCicloEscolarModeloLista() {
@@ -73,4 +83,5 @@ public class CiclosEscolaresControlador {
     public void setHtmlDataTable(HtmlDataTable htmlDataTable) {
         this.htmlDataTable = htmlDataTable;
     }
+
 }
