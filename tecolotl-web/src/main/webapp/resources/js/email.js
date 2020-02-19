3c5ebd5539b9d8c0693c3115337f35add30c132e
Mauricio <maugg76@gmail.com>
2020-02-19 09:53:27 -0600

    mapa mental 10 y probar email

--- tecolotl-web/src/main/webapp/resources/js/email.js
+++ tecolotl-web/src/main/webapp/resources/js/email.js
@@ -25,9 +25,7 @@ document.addEventListener("DOMContentLoaded",function () {
     function sendEmail() {
         var contactos = document.querySelectorAll('#seccion3 div > input');
         Email.send({
-            Host : "in-v3.mailjet.com",
-            Username : "f8fb25bef858388aea9dedeb6042676d",
-            Password : "64705e76a3d268cd48ba0784fdb235b4",
+            SecureToken: "96f1d275-0ac3-45ea-a6bb-0f84511e6843",
             To : 'e-squadron@e-squadron.com.mx',
             From : "maugg76@gmail.com",
             Subject : "Contacto Escuela",
