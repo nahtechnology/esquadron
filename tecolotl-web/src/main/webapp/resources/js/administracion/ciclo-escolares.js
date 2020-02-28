2006716c6853bcdea68187a0639ca46cce745399
antonioalonso <antonio.alonso@nahtechnology.com>
2020-02-27 21:53:20 -0600

    ciclo escolar: activa - desactiva

--- tecolotl-web/src/main/webapp/resources/js/administracion/ciclo-escolares.js
+++ tecolotl-web/src/main/webapp/resources/js/administracion/ciclo-escolares.js
@@ -1,11 +1,12 @@
 let modalCicloEscolar;
+let modalActivar;
 
 document.addEventListener('DOMContentLoaded', inicio);
 
 function inicio(evento) {
     modalCicloEscolar = document.getElementById('modal-escuela');
+    modalActivar = document.getElementById('modal-activar');
 }
-
 
 function cierraModal(data, modal) {
     if (data.status === 'success') {
@@ -15,8 +16,23 @@ function cierraModal(data, modal) {
                     UIkit.modal(modalCicloEscolar).hide();
                 }
                 break;
+            case 'modalActivar':
+                if (modalActivar.querySelector('.uk-alert-danger') === null) {
+                    UIkit.modal(modalActivar).hide();
+                }
+                break;
             default:
                 console.error('Opcion un no programada:' + modal);
         }
+    }
+}
+
+function abreModal(modal) {
+    switch (modal) {
+        case 'modalActivar':
+            modalActivar.querySelector('.uk-modal-header').click();
+            break;
+        default:
+            console.log('Opcion aun no programada' + modal);
     }
 }
