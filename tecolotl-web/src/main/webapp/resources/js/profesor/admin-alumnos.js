8bca4398aa1d09f811422487a7f181373f2f30b2
mauricio <maugg76@gmail.com>
2020-03-25 20:16:23 -0600

    admin alumnos reporte

--- tecolotl-web/src/main/webapp/resources/js/profesor/admin-alumnos.js
+++ tecolotl-web/src/main/webapp/resources/js/profesor/admin-alumnos.js
@@ -1,12 +1,11 @@
 var tabla;
 var alumnos = [];
-var botonResporte = document.querySelector('.uk-container button.reporte');
+
 var tablaAlumnos = document.getElementById('formulario-tabla-alumno:tabla-alumno');
 
 document.addEventListener('DOMContentLoaded', function (evento) { 
     tabla = document.querySelector('.uk-container table');
     //formularioNuevoAlumno.querySelector('.uk-modal-body div:nth-child(5) input').addEventListener('input', entradaFecha);
-    botonResporte.addEventListener('click', generarReporte);
     if(document.querySelector('#tabla-vacia') === null){
         desabilitar();
         botonResporte.disabled = false;
@@ -29,23 +28,7 @@ function limpiaFormulario(evento) {
     })
 }
 
-function generarReporte(evento) {
-    // console.log(evento);
-    // tabla = document.getElementById('formulario-tabla-alumno:tabla-alumno');
-    // csv = Array.from(tabla.tHead.rows[0].cells).map(th => th.textContent).join(',').concat('\n');
-    // console.log(csv);
-    // for (i = 0; i < tabla.tBodies[0].rows.length; i++) {
-    //     csv += Array.from(tabla.tBodies[0].rows[i].cells).map(td => td.querySelector('span').textContent.trim() === '' ? 'N/A' : td.querySelector('span').textContent).join(',').concat('\n');
-    // }
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    var grupo = urlParams.get('grupo');
-    var hiddenElement = document.createElement('a');
-    console.log(grupo);
-    UIkit.notification("The document is being downloaded, please wait");
-    hiddenElement.href = "http://reportes.e-squadron.com.mx/reporte-alumnos?grupo=".concat(grupo);
-    hiddenElement.click();
-}
+
 
 function desabilitar() {
     var filas = tablaAlumnos.querySelectorAll('tbody tr');
