8f315017054db76375e681168a9beca2d9700280
nahtechnology <antonio.alonso@nahtechnology.com>
2020-05-14 13:04:50 -0500

    Se crean los ORM para la tabla de notificaciones

---
+++ tecolotl-profesor/src/main/java/tecolotl/profesor/entidad/NotificacionEntidad.java
@@ -0,0 +1,68 @@
+package tecolotl.profesor.entidad;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(schema = "profesor", name = "notificacion")
+public class NotificacionEntidad {
+
+    private NotificacionEntidadPK notificacionEntidadPK;
+    private Date momento;
+    private boolean leido;
+
+    @EmbeddedId
+    public NotificacionEntidadPK getNotificacionEntidadPK() {
+        return notificacionEntidadPK;
+    }
+
+    public void setNotificacionEntidadPK(NotificacionEntidadPK notificacionEntidadPK) {
+        this.notificacionEntidadPK = notificacionEntidadPK;
+    }
+
+    @Basic
+    @Column(name = "momento", insertable = false)
+    @Temporal(TemporalType.DATE)
+    public Date getMomento() {
+        return momento;
+    }
+
+    public void setMomento(Date momento) {
+        this.momento = momento;
+    }
+
+    @Basic
+    @Column(name = "leido", insertable = false)
+    public boolean isLeido() {
+        return leido;
+    }
+
+    public void setLeido(boolean leido) {
+        this.leido = leido;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificacionEntidad that = (NotificacionEntidad) o;
+        return notificacionEntidadPK.equals(that.notificacionEntidadPK);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(notificacionEntidadPK);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("NotificacionEntidad{");
+        sb.append("notificacionEntidadPK=").append(notificacionEntidadPK);
+        sb.append(", momento=").append(momento);
+        sb.append(", leido=").append(leido);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
