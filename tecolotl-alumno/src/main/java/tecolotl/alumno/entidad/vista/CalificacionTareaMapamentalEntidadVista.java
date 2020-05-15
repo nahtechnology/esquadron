630658cfd60f5ce4b8499477332b77e696212884
nahtechnology <antonio.alonso@nahtechnology.com>
2020-05-15 18:19:38 -0500

    Ahora cuando califica una tarea se mapa mental, se pueden visualizar las calificaciones

---
+++ tecolotl-alumno/src/main/java/tecolotl/alumno/entidad/vista/CalificacionTareaMapamentalEntidadVista.java
@@ -0,0 +1,55 @@
+package tecolotl.alumno.entidad.vista;
+
+import org.hibernate.annotations.Immutable;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Immutable
+@Table(schema = "alumno", name = "calificacion_tarea_mapamental")
+@NamedQuery(
+        name = "CalificacionTareaMapamentalEntidadVista.busca",
+        query = "SELECT ctm FROM CalificacionTareaMapamentalEntidadVista ctm WHERE ctm.idTarea = :idTarea"
+)
+public class CalificacionTareaMapamentalEntidadVista {
+
+    private Short cardinalidad;
+    private UUID idTarea;
+    private Integer count;
+    private Short puntaje;
+
+    @Id
+    public Short getCardinalidad() {
+        return cardinalidad;
+    }
+
+    public void setCardinalidad(Short cardinalidad) {
+        this.cardinalidad = cardinalidad;
+    }
+
+    @Column(name = "id_tarea")
+    public UUID getIdTarea() {
+        return idTarea;
+    }
+
+    public void setIdTarea(UUID idTarea) {
+        this.idTarea = idTarea;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Short getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(Short puntaje) {
+        this.puntaje = puntaje;
+    }
+}
