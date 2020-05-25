d7256d4b51e9c5ba893bee697c814b2ab35edb1b
nahtechnology <nahtechnology@github.com>
2020-05-25 12:44:48 -0500

    elimina y actualiza notificaciones en el buzon

--- tecolotl-web/src/main/java/tecolotl/web/profesor/BuzonControlador.java
+++ tecolotl-web/src/main/java/tecolotl/web/profesor/BuzonControlador.java
@@ -5,14 +5,19 @@ import tecolotl.profesor.sesion.NotificacionSesionBean;
 
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.logging.Logger;
 
-@RequestScoped
+@ViewScoped
 @Named
-public class BuzonControlador {
+public class BuzonControlador implements Serializable {
 
     @Inject
     private NotificacionSesionBean notificacionSesionBean;
@@ -25,12 +30,27 @@ public class BuzonControlador {
 
     private List<NotificacionModelo> notificacionModeloLista;
 
+    @PostConstruct
     public void inicio() {
         notificacionModeloLista = notificacionSesionBean.busca(profesorControlador.getProfesorModelo().getId());
     }
 
     public void leido(String idAlumno) {
-        logger.info(idAlumno);
+        int cambios =
+                notificacionSesionBean.leido(profesorControlador.getProfesorModelo().getId(), UUID.fromString(idAlumno));
+        if (cambios != 0) {
+            Optional<NotificacionModelo> notificacionModeloOptional =
+                notificacionModeloLista.stream().filter(not -> not.getIdAlumno().compareTo(UUID.fromString(idAlumno)) == 0).findFirst();
+            notificacionModeloOptional.ifPresent(notificacionModelo -> notificacionModelo.setLeido(true));
+        }
+    }
+
+    public void elimina(String idAlumno) {
+        int cambios =
+                notificacionSesionBean.elimina(profesorControlador.getProfesorModelo().getId(), UUID.fromString(idAlumno));
+        if (cambios != 0) {
+            notificacionModeloLista.removeIf(notificacionModelo -> notificacionModelo.getIdAlumno().compareTo(UUID.fromString(idAlumno)) == 0);
+        }
     }
 
     public List<NotificacionModelo> getNotificacionModeloLista() {
