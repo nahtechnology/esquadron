b136dfe00bce825834e09c0eace1e57f478fc2cf
mauricio <maugg76@gmail.com>
2020-07-15 16:39:01 -0500

    promedio de tareas vista tarea-alumno y mostrar puntajes de los progress bar al eliminar taeas

--- tecolotl-web/src/main/webapp/resources/js/profesor/tarea-alumno-tabla.js
+++ tecolotl-web/src/main/webapp/resources/js/profesor/tarea-alumno-tabla.js
@@ -1,17 +1,13 @@
-document.addEventListener('DOMContentLoaded',()=>{
+let nivelesSet = new Set();
+let nivelActual = document.querySelector('.elimina .uk-card-header > div > div:last-child > p:last-child').innerText.split(':')[1].trim();
 
+document.addEventListener('DOMContentLoaded',()=>{
     let seleccionFiltro = document.querySelector('#filtro-nivel');
     let nivelesTabla = document.querySelectorAll('.tabla-alumnos td > img');
-    let nivelesSet = new Set();
     let indiceOpcion;
     let conteoOpcion = 0, piboteOpcion = 0;
-    let nivelActual = document.querySelector('.elimina .uk-card-header > div > div:last-child > p:last-child').innerText.split(':')[1].trim();
-    let tablaAlumnosCuerpo = document.querySelector('.tabla-alumnos > tbody');
-    let nodoAsignadas = document.querySelector('#tareas-asignadas > span');
-    let nodoPromediadas = document.querySelector('#tareas-promediadas > span');
-    let nodoPromedioGeneral = document.querySelector('#promedio-general > span');
-    let datosVista = new PromedioGeneral(tablaAlumnosCuerpo,nodoAsignadas,nodoPromediadas,nodoPromedioGeneral);
-    datosVista.promedio2();
+
+    barrasValor();
     switch (nivelActual) {
         case "A1":
             indiceOpcion = "1";
@@ -29,6 +25,30 @@ document.addEventListener('DOMContentLoaded',()=>{
             indiceOpcion = "5";
             break;
     }
+    crearNiveles(nivelesTabla);
+    console.log(conteoOpcion);
+    nivelesSet.forEach((opcion) =>{
+       let etiqueta = document.createElement('option');
+       etiqueta.value=opcion;
+       etiqueta.innerText = `level ${opcion}`;
+       seleccionFiltro.appendChild(etiqueta);
+       if(nivelActual === opcion){
+           piboteOpcion = conteoOpcion + 1;
+       }
+       conteoOpcion++;
+    });
+
+    seleccionFiltro.addEventListener('change',(evt)=>{
+        let nivel = `hover-${evt.target.value}`;
+        console.log(nivel);
+        verFilas(nivel);
+    });
+    console.log(piboteOpcion);
+    seleccionFiltro.selectedIndex = `${piboteOpcion}`;
+    verFilas(`hover-${nivelActual}`);
+
+});
+function crearNiveles(nivelesTabla) {
     nivelesTabla.forEach(nivel => {
         let nivelActividad = nivel.dataset.nivel;
         nivelesSet.add(nivelActividad);
@@ -53,38 +73,26 @@ document.addEventListener('DOMContentLoaded',()=>{
                 break;
         }
     });
-    console.log(conteoOpcion);
-    nivelesSet.forEach((opcion) =>{
-       let etiqueta = document.createElement('option');
-       etiqueta.value=opcion;
-       etiqueta.innerText = `level ${opcion}`;
-       seleccionFiltro.appendChild(etiqueta);
-       if(nivelActual === opcion){
-           piboteOpcion = conteoOpcion + 1;
-       }
-       conteoOpcion++;
-    });
-
-    seleccionFiltro.addEventListener('change',(evt)=>{
-        let cuerpoTabla = document.querySelector('.tabla-alumnos');
-        let filas = document.querySelectorAll('.tabla-alumnos tbody > tr');
-        let nivel = `hover-${evt.target.value}`;
-        if (cuerpoTabla.querySelector('tbody .fila-vacia') !== null ){
-            let numeroFila = cuerpoTabla.querySelector('tbody .fila-vacia').rowIndex;
-            cuerpoTabla.deleteRow(numeroFila);
-        }
-        cuerpoTabla.classList.add('visibilidad-fila');
-        setTimeout(animacionFilas,500,filas,nivel,cuerpoTabla);
-    });
-    console.log(piboteOpcion);
-    seleccionFiltro.selectedIndex = `${piboteOpcion}`;
-
-
-});
+}
+function verFilas(nivel) {
+    let cuerpoTabla = document.querySelector('.tabla-alumnos');
+    let filas = document.querySelectorAll('.tabla-alumnos tbody > tr');
+    if (cuerpoTabla.querySelector('tbody .fila-vacia') !== null ){
+        let numeroFila = cuerpoTabla.querySelector('tbody .fila-vacia').rowIndex;
+        cuerpoTabla.deleteRow(numeroFila);
+    }
+    cuerpoTabla.classList.add('visibilidad-fila');
+    setTimeout(animacionFilas,500,filas,nivel,cuerpoTabla);
+}
 
 function animacionFilas(filas,nivel,tabla) {
     let cuenta = 0;
     let cuerpo = tabla.querySelector('tbody');
+    let tablaAlumnosCuerpo = document.querySelector('.tabla-alumnos > tbody');
+    let nodoAsignadas = document.querySelector('#tareas-asignadas > span');
+    let nodoPromediadas = document.querySelector('#tareas-promediadas > span');
+    let nodoPromedioGeneral = document.querySelector('#promedio-general > span');
+    let datosVista = new PromedioGeneral(tablaAlumnosCuerpo,nodoAsignadas,nodoPromediadas,nodoPromedioGeneral,nivel);
     console.log(nivel);
     filas.forEach(fila => {
         if (nivel === "hover-todos"){
@@ -107,6 +115,33 @@ function animacionFilas(filas,nivel,tabla) {
         row.classList.add('fila-vacia');
     }
     tabla.classList.remove('visibilidad-fila');
+    datosVista.promedio2();
+}
+function barrasValor() {
+    let progreso = document.getElementById("formulario-tabla-actividades:tabla-actividades");
+    let filas = progreso.querySelectorAll('tbody tr > td');
+    let barra = [];
+    filas.forEach(function (fila) {
+        if(fila.querySelector('progress')){
+            barra.push(fila);
+        }
+    });
+    barra.forEach(function (progres) {
+        let valor = document.createElement('span');
+        valor.innerHTML = progres.querySelector('progress').value + '%';
+        progres.insertBefore(valor,progres.querySelector('progress'));
+    });
+}
+function renderizarBorrado(data) {
+    let seleccionFiltro = document.querySelector('#filtro-nivel');
+    if (data.status === "success"){
+        let nivelesTabla = document.querySelectorAll('.tabla-alumnos td > img');
+        let nivel = `hover-${seleccionFiltro.value}`;
+        console.log(nivel);
+        barrasValor();
+        crearNiveles(nivelesTabla);
+        verFilas(nivel);
+    }
 }
 
 class PromedioTarea {
@@ -140,16 +175,20 @@ class PromedioTarea {
 }
 
 class PromedioGeneral {
-    constructor(tabla,tareasAsignadas,tareasPromediadas,promedioGeneral) {
+    constructor(tabla,tareasAsignadas,tareasPromediadas,promedioGeneral,nivel) {
         this.tabla = tabla;
         this.tareasAsignadas = tareasAsignadas;
         this.tareasPromediadas = tareasPromediadas;
         this.promedioGeneral = promedioGeneral;
+        this.nivel = nivel;
     }
     promedio2(){
-        let filas = this.tabla.querySelectorAll('tr');
+        let filas;
         let promedios = [];
         let dividendo = 0;
+        if (this.nivel === "hover-todos"){
+           filas = this.tabla.querySelectorAll("tr");
+        }else { filas = this.tabla.querySelectorAll(`tr.${this.nivel}`)}
         filas.forEach(fila =>{
             let columnas = fila.querySelectorAll('td');
             let promedioFila = new PromedioTarea(columnas[2].dataset.transcrip,columnas[3].dataset.gramatica,columnas[4].dataset.mapaMental,columnas[5].dataset.relacionarImagen,columnas[6].dataset.completarOracion,columnas[7].dataset.ordenarOraciones,columnas[8].dataset.relacionarOraciones);
@@ -159,12 +198,14 @@ class PromedioGeneral {
                 console.log(promedioFila.promedio());
             }
         });
-        promedios.forEach(total => {
-            dividendo += total;
-        })
+        if (promedios.length > 0){
+            promedios.forEach(total => {
+                dividendo += total;
+            });
+        }
         this.tareasAsignadas.innerText = filas.length;
         this.tareasPromediadas.innerText = promedios.length;
-        this.promedioGeneral.innerText = Math.round(dividendo/promedios.length);
+        this.promedioGeneral.innerText = promedios.length > 0 ? Math.round(dividendo/promedios.length) : "No hay tareas completadas";
     }
 
 }
