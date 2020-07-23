cc68bb176b8ad1d9782a0bee1dfa34d6bcbe1347
mauricio <maugg76@gmail.com>
2020-07-22 20:36:26 -0500

    se modifica colocan estilos coordinador detalles-profesor icono de descarga, mensaje en modulo profesor estilos y estilos input checkbox en el modulo de administracion

--- tecolotl-web/src/main/webapp/resources/js/profesor/licencia.js
+++ tecolotl-web/src/main/webapp/resources/js/profesor/licencia.js
@@ -3,15 +3,21 @@ document.addEventListener('DOMContentLoaded',()=>{
    let permitir = sessionStorage.getItem('mensaje');
    if (parseInt(diasLicencias) <= 15 && permitir === "true"){
         let nodo = document.createElement('div');
+         let titulo = document.createElement('span');
+        let letrero = document.createElement('span');
         let nodoreferencia = document.querySelector('h1');
         let nodoPadre = nodoreferencia.parentElement;
         let icon = document.createElement('span');
-        nodo.innerText = `En ${parseInt(diasLicencias)} dias se termina tu licencia`;
+        letrero.innerText = `En ${parseInt(diasLicencias)} dias se termina tu licencias`;
         nodo.classList.add('mensaje-licencias');
         icon.setAttribute('uk-icon','close');
         icon.setAttribute('uk-tooltip','No mostrar mensaje')
         icon.classList.add('cerrar-mensaje');
         nodo.appendChild(icon);
+       titulo.innerText ="IMPORTANTE";
+       titulo.style.marginBottom = "8px";
+       nodo.appendChild(titulo);
+       nodo.appendChild(letrero);
         nodoPadre.insertBefore(nodo,nodoreferencia);
         icon.addEventListener('click',()=>{
             nodo.remove();
