de3885e91a68de24a668489a5e6264ed105c773a
mauricio <maugg76@gmail.com>
2020-08-24 18:16:43 -0500

    se agrega formulario landing y estilos vista administracion heredar-alumno.xhtml

--- tecolotl-web/src/main/webapp/resources/js/administracion/heredar-alumno.js
+++ tecolotl-web/src/main/webapp/resources/js/administracion/heredar-alumno.js
@@ -1,7 +1,33 @@
+
 document.addEventListener("DOMContentLoaded", () => {
+   eventoCheckbox();
     limpiaSeleccion();
 });
 
 function limpiaSeleccion() {
     document.querySelectorAll('select').forEach(sel => sel.removeAttribute('size'));
+}
+function eventoCheckbox(){
+    console.log('entro funcion');
+    let checkboxGeneral = document.querySelector('.switch > input[type=checkbox]');
+    let checkboxs = document.querySelectorAll('.tabla-checkbox input[type=checkbox]');
+    checkboxGeneral.addEventListener('change',(evt)=>{
+        if (evt.target.checked === true){
+            checkboxs.forEach(check =>{
+                check.checked = true;
+            });
+        }else {
+            checkboxs.forEach(check =>{
+                check.checked = false;
+            });
+        }
+
+    });
+}
+function tablaCargada(){
+    let tablaChekbox;
+    tablaChekbox = document.querySelector('.tabla-checkbox');
+    tablaChekbox.onload = ()=>{
+        console.log('se termino de cargar');
+    }
 }
