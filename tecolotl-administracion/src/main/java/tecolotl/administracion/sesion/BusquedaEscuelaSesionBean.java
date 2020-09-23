d16f062e0886c9d611d06c8c1cf3b96707448c90
nahtechnology <nahtechnology@github.com>
2020-09-23 12:40:01 -0500

    se recuelven las incidencias: Remueve el grupo cuando se hereda alumno Ahora cuando buscas un alumno ahora lo case lower case

--- tecolotl-administracion/src/main/java/tecolotl/administracion/sesion/BusquedaEscuelaSesionBean.java
+++ tecolotl-administracion/src/main/java/tecolotl/administracion/sesion/BusquedaEscuelaSesionBean.java
@@ -21,9 +21,9 @@ public class BusquedaEscuelaSesionBean {
         Query query = entityManager.createNativeQuery("SELECT e.clave_centro_trabajo,a.nombre,a.apellido_paterno,a.apellido_materno," +
                 "p.apodo,g.grado,g.grupo,ce.descripcion FROM administracion.escuela e JOIN profesor.ciclo_escolar ce ON e.clave_centro_trabajo = ce.id_escuela JOIN " +
                 "profesor.grupo g on ce.inicio = g.inicio and ce.fin = g.fin and ce.id_escuela = g.id_escuela JOIN profesor.profesor p ON g.id_profesor = p.id JOIN " +
-                "profesor.grupo_alumno ga on g.id = ga.id_grupo JOIN alumno.alumno a ON ga.id_alumno = a.id WHERE e.clave_centro_trabajo = ? AND a.nombre LIKE ?");
+                "profesor.grupo_alumno ga on g.id = ga.id_grupo JOIN alumno.alumno a ON ga.id_alumno = a.id WHERE e.clave_centro_trabajo = ? AND LOWER (a.nombre) LIKE ?");
         query.setParameter(1, claveCentroTrabajo);
-        query.setParameter(2, "%".concat(nombre).concat("%"));
+        query.setParameter(2, "%".concat(nombre.toLowerCase()).concat("%"));
         List<Object[]> lista = (List<Object[]>)query.getResultList();
         for (Object[] objecto : lista) {
             EscuelaAlumnoModelo escuelaAlumnoModelo = new EscuelaAlumnoModelo();
